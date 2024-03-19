e01b6b7e1c66f757a1d23f3f6ae5f34239a6dec9
Devin Bolden <devinbolden6@gmail.com>
2024-03-18 20:28:42 -0500

    added bottom graph html/WIP buttons

--- src/components/pages/Landing/RenderLandingPage.jsx
+++ src/components/pages/Landing/RenderLandingPage.jsx
@@ -8,7 +8,7 @@ import '../../../styles/RenderLandingPage.less';
 import { Button } from 'antd';
 import { useHistory } from 'react-router-dom';
 // for the purposes of testing PageNav
-// import PageNav from '../../common/PageNav';
+import PageNav from '../../common/PageNav';
 
 function RenderLandingPage(props) {
   const scrollToTop = () => {
@@ -31,7 +31,7 @@ function RenderLandingPage(props) {
         </div>
       </div>
 
-      {/* Graphs Section: Add code here for the graphs section for your first ticket */}
+      {/*START HEAD GRAPH SECTION */}
       <div className="graphs-section">
         <div className="grant-rates-by-office-graph-container">
           <img
@@ -58,8 +58,8 @@ function RenderLandingPage(props) {
           <p>Search Grant Rates Over Time</p>
         </div>
       </div>
+      {/**END OF HEAD GRAPH SECTION */}
 
-      {/* <div className="graphs-section"> */}
       <div className="view-more-data-btn-container">
         <Button
           type="default"
@@ -67,6 +67,15 @@ function RenderLandingPage(props) {
           onClick={() => history.push('/graphs')}
         >
           View the Data
+        </Button>
+      </div>
+      <div className="download-the-data-btn-container">
+        <Button
+          type="default"
+          style={{ backgroundColor: '#404C4A', color: '#FFFFFF' }}
+          onClick={() => history.push('/graphs')}
+        >
+          Download the Data
         </Button>
       </div>
 
@@ -87,8 +96,37 @@ function RenderLandingPage(props) {
         </div>
       </div>
       <div>
-        {/* Bottom Section: Add code here for the graphs section for your first ticket */}
-        {/* <div className="bottom-section">*/}
+        {/*START BOTTOM GRAPH SECTION */}
+        <div className="bottom-section">
+          <h1>Systemic Disparity Insights</h1>
+          <div class="data-container">
+            <div class="first-data-point-container">
+              <h2>36%</h2>
+              <h3>
+                By the end of the Trump administration, the average asylum
+                office grant rate had fallen 36 percent from an average of 44
+                percent in fiscal year 2016 to 28 percent in fiscal year 2020.
+              </h3>
+            </div>
+            <div class="second-data-point-container">
+              <h2>5%</h2>
+              <h3>
+                The New York asylum office grant rate dropped to 5 percent in
+                fiscal year 2020.
+              </h3>
+            </div>
+            <div class="third-data-point-container">
+              <h2>6x Lower</h2>
+              <h3>
+                Between fiscal year 2017 and 2020, the New York asylum office’s
+                average grant rate was six times lower than the San Francisco
+                asylum office.
+              </h3>
+            </div>
+          </div>
+        </div>
+        {/*END BOTTOM GRAPH SECTION */}
+
         <p onClick={() => scrollToTop()} className="back-to-top">
           Back To Top ^
         </p>
